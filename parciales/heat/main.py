f70506fcf5a9dc058316976d5b607d097f383ff4
freddyeli <freddyelicampillo@gmail.com>
2023-04-21 22:20:43 -0600

    q

---
+++ parciales/heat/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+t0 = 0
+tf = 1
+T0 = 0
+Tl = 1
+l = 1
+m = 10
+n = 10
+h = (tf - t0)/n
+k = l/(m+2)
+
+my_matrix = np.empty((n+1,m+2))
+
+x = []
+t = []
+
+for i in range(m+3)
+    x[i]= i*k
+
